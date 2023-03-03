76cfa063b538ffeab679c5df9fb1e0de1d9d887b
anmamimu <annapanman42899@gmail.com>
2023-03-03 23:30:31 +0900

    fix: configの箇所移動

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,12 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 
 config.assets.initialize_on_precompile = false
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 module Nitibu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
